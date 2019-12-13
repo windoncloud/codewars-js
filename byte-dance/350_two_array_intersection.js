2eb3fdb7ae6bc473656e7fe02aa3c752d107b6de
Kingsley <448409934@qq.com>
2019-12-13 23:53:51 +0800

    two array intersection log

--- byte-dance/350_two_array_intersection.js
+++ byte-dance/350_two_array_intersection.js
@@ -138,3 +138,5 @@ var intersect = function(nums1, nums2) {
 // 链接：https://leetcode-cn.com/problems/intersection-of-two-arrays-ii/solution/js-xie-leetcode-by-zhl1232/
 //     来源：力扣（LeetCode）
 // 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+console.log('intersect', intersect())
