420e90bc8d4cdfafddde87e23a606c6a929f858f
Kingsley <448409934@qq.com>
2019-12-18 00:25:14 +0800

    max profit

--- byte-dance/121_best-time-to-buy-and-sell-stock.js
+++ byte-dance/121_best-time-to-buy-and-sell-stock.js
@@ -1,10 +1,12 @@
 
-var Solution = function (prices = []) {
+let arr = [7, 1, 5, 3, 6, 4]
+
+let solution = function (prices = []) {
     let maxprofit = 0;
     for (let i = 0; i < prices.length - 1; i++) {
         for (let j = i + 1; j < prices.length; j++) {
             let profit = prices[j] - prices[i];
-            if (profit > maxprofit)
+            if (profit > maxprofit) {
                 maxprofit = profit;
             }
         }
@@ -12,18 +14,19 @@ var Solution = function (prices = []) {
     return maxprofit;
 }
 
-public class Solution {
-    public int maxProfit(int prices[]) {
-    int minprice = Integer.MAX_VALUE;
-    int maxprofit = 0;
-    for (int i = 0; i < prices.length; i++) {
-    if (prices[i] < minprice)
-    minprice = prices[i];
-    else if (prices[i] - minprice > maxprofit)
-    maxprofit = prices[i] - minprice;
-}
-return maxprofit;
-}
+let solution2 = function (prices = []) {
+    let minprice = Infinity;
+    let maxprofit = 0;
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minprice) {
+            minprice = prices[i];
+        } else if (prices[i] - minprice > maxprofit) {
+            maxprofit = prices[i] - minprice;
+        }
+    }
+    return maxprofit;
 }
 
+console.log('arr1', solution(arr))
+console.log('arr2', solution2(arr))
 // https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/solution/mai-mai-gu-piao-de-zui-jia-shi-ji-by-leetcode/
