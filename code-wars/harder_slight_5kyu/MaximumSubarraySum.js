e0292bb6dc0754103cb76d83e8a3a04570956079
Kingsley <448409934@qq.com>
2019-12-20 23:57:09 +0800

    we

--- code-wars/harder_slight_5kyu/MaximumSubarraySum.js
+++ code-wars/harder_slight_5kyu/MaximumSubarraySum.js
@@ -20,11 +20,11 @@ function maxSequence (arr){
 
 // better
 var maxSequence = function(arr){
-    var min = 0, ans = 0, i, sum = 0;
-    for (i = 0; i < arr.length; ++i) {
-        sum += arr[i];
-        min = Math.min(sum, min);
-        ans = Math.max(ans, sum - min);
-    }
-    return ans;
+var min = 0, ans = 0, i, sum = 0;
+for (i = 0; i < arr.length; ++i) {
+    sum += arr[i];
+    min = Math.min(sum, min);
+    ans = Math.max(ans, sum - min);
+}
+return ans;
 }
