a4eb1f4206bb8d9d7e137fc9b7eff93935c44178
Kingsley <448409934@qq.com>
2020-05-24 15:52:31 +0800

    边界索引变更，二分查找

--- js-interview/halfSearch.js
+++ js-interview/halfSearch.js
@@ -1,4 +1,4 @@
-// 二分查找、前提条件：有序
+// 二分查找、边界[], 左右闭合,前提条件：有序
 function halfSearch(arr = [1, 2, 3, 4, 5, 7, 9], target = 5) {
     let left = 0, right = arr.length -1; // 在 [left, ..., right] 范围内寻找 target (循环不变量)
     while (left <= right) { // 当left == right 时，区间[left, ..., right] 是有效的
@@ -9,6 +9,22 @@ function halfSearch(arr = [1, 2, 3, 4, 5, 7, 9], target = 5) {
             right = mid - 1 // [left, ... ,mid - 1]
         } else {
             return mid
+        }
+    }
+    return -1
+}
+
+// 二分查找边界变化，左闭右开，[)
+function halfSearch2(arr = [1, 2, 3, 4, 5, 7, 9], target = 5) {
+    let left = 0, right = arr.length; // 在 [left, ..., right) 范围内寻找 target (循环不变量)
+    while (left < right) { // 当left == right 时，区间[left, ..., right) 是有效的, [42, 42)无效，故不能等于
+        let mid = Math.floor((left + right)/ 2) // 中间数组索引
+        if(arr[mid] === target) {
+            return mid
+        } else if (arr[mid] < target) {
+            left = mid + 1 //  target 在 [mid + 1, ... ,right) 中
+        } else {   // if(arr[mid] > target)
+            right = mid // target 在 [left, ... ,mid) 中
         }
     }
     return -1
