e9f33ecabcd337853369a2c708a0622692fda705
Kingsley <448409934@qq.com>
2020-05-24 15:26:57 +0800

    术语：循环不变量

--- js-interview/halfSearch.js
+++ js-interview/halfSearch.js
@@ -1,6 +1,6 @@
 // 二分查找、前提条件：有序
 function halfSearch(arr = [1, 2, 3, 4, 5, 7, 9], target = 5) {
-    let left = 0, right = arr.length -1; // 在 [left, ..., right] 范围内寻找 target
+    let left = 0, right = arr.length -1; // 在 [left, ..., right] 范围内寻找 target (循环不变量)
     while (left <= right) { // 当left == right 时，区间[left, ..., right] 是有效的
         let mid = Math.floor((left + right)/ 2) // 中间数组索引
         if (arr[mid] < target) {
