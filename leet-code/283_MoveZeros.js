39e79c9f12c2e1b919b278c53d154f5ba692f542
Kingsley <448409934@qq.com>
2020-05-28 08:45:53 +0800

    0 元素后移

--- leet-code/283_MoveZeros.js
+++ leet-code/283_MoveZeros.js
@@ -4,11 +4,16 @@ function MoveZeroes (nums = [1,0,2,0,3,0,0,5,0]) {
     let k = 0; // nums中,[0...K)均为非0元素
     for (let i =0; i<nums.length; i++) {
         if(nums[i]) {
+            // 1
+            // swap(nums, k, i)
+            // k++
+            // 2
             if(i !== k) {
                 // swap(nums[k++], nums[i])
                 swap(nums, k, i)
                 k++
             } else {
+                // 索引相同说明前面k项都为0，此时不交换,直接++完事
                 k++
             }
         }
