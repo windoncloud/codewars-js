56080bf1eed63b9653126f6cafafa77893befa3a
Kingsley <448409934@qq.com>
2020-06-17 08:51:47 +0800

    same tree

---
+++ leet-code/100_SameTree.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+https://leetcode-cn.com/problems/same-tree/solution/jsdi-gui-by-285008875/
+var isSameTree = function(p, q) {
+    if(p === null && q === null) return true
+    if(p === null || q === null) return false // 不能一个为空一个不为空
+    if(p.val !== q.val){
+        return false;
+    }
+    return isSameTree(p.left, q.left) && isSameTree(p.right,q.right);
+};
