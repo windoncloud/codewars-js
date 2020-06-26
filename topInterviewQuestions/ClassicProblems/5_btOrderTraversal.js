fc16c9d94e0c0c1f325f6b6468f3953e671f1540
Kingsley <448409934@qq.com>
2020-06-26 23:29:56 +0800

    fet: binary tree order traversal

---
+++ topInterviewQuestions/ClassicProblems/5_btOrderTraversal.js
@@ -0,0 +1,32 @@
+// https://leetcode-cn.com/problems/binary-tree-level-order-traversal
+/*
+    给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+    示例：
+    二叉树：[3,9,20,null,null,15,7],
+    3
+    / \
+      9  20
+    /  \
+       15   7
+    返回其层次遍历结果：
+    [
+        [3],
+        [9,20],
+        [15,7]
+    ]
+    链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal
+*/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+
+};
