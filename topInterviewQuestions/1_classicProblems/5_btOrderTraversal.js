47035ad1e4b5ad24644eb82b262b430db641a21c
Kingsley <448409934@qq.com>
2020-07-01 07:05:25 +0800

    fet: tree node generator

--- topInterviewQuestions/1_classicProblems/5_btOrderTraversal.js
+++ topInterviewQuestions/1_classicProblems/5_btOrderTraversal.js
@@ -5,9 +5,9 @@
     二叉树：[3,9,20,null,null,15,7],
     3
     / \
-      9  20
-    /  \
-       15   7
+   9  20
+      /  \
+     15   7
     返回其层次遍历结果：
     [
         [3],
@@ -16,6 +16,9 @@
     ]
     链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal
 */
+// BFS 的使用场景总结：层序遍历、最短路径问题 与DFS的比较
+// https://leetcode-cn.com/problems/binary-tree-level-order-traversal/solution/bfs-de-shi-yong-chang-jing-zong-jie-ceng-xu-bian-l/
+// 二叉树的层序遍历
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
@@ -27,6 +30,23 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
+// 链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal/solution/er-cha-shu-de-ceng-xu-bian-li-by-leetcode-solution/
 var levelOrder = function(root) {
+    const ret = [];
+    if (!root) return ret;
 
+    const q = [];
+    q.push(root);
+    while (q.length !== 0) {
+        const currentLevelSize = q.length;
+        ret.push([]);
+        for (let i = 1; i <= currentLevelSize; ++i) {
+            const node = q.shift();
+            ret[ret.length - 1].push(node.val);
+            if (node.left) q.push(node.left);
+            if (node.right) q.push(node.right);
+        }
+    }
+
+    return ret;
 };
