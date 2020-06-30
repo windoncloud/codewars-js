1796343ae2d42f3002fefd001247b161c4642cba
Kingsley <448409934@qq.com>
2020-07-01 07:17:52 +0800

    fet: runner added exam

--- topInterviewQuestions/1_classicProblems/5_btOrderTraversal.js
+++ topInterviewQuestions/1_classicProblems/5_btOrderTraversal.js
@@ -40,7 +40,7 @@ var levelOrder = function(root) {
     while (q.length !== 0) {
         const currentLevelSize = q.length;
         ret.push([]);
-        for (let i = 1; i <= currentLevelSize; ++i) {
+        for (let i = 1; i <= currentLevelSize; ++i) { // i++
             const node = q.shift();
             ret[ret.length - 1].push(node.val);
             if (node.left) q.push(node.left);
