b41e05d99f2f69e665910b0434214e67e2e5666f
Kingsley <448409934@qq.com>
2020-07-08 09:05:31 +0800

    fet: climb stairs

--- topInterviewQuestions/11_dynamicProgramming/3_climbStairs.js
+++ topInterviewQuestions/11_dynamicProgramming/3_climbStairs.js
@@ -1,3 +1,4 @@
+// 70. 爬楼梯
 // 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
 // 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
 // 注意：给定 n 是一个正整数。
@@ -14,10 +15,19 @@
 // 1.  1 阶 + 1 阶 + 1 阶
 // 2.  1 阶 + 2 阶
 // 3.  2 阶 + 1 阶
+// https://leetcode-cn.com/problems/climbing-stairs/solution/hua-jie-suan-fa-70-pa-lou-ti-by-guanpengchn/
 /**
  * @param {number} n
  * @return {number}
  */
 var climbStairs = function(n) {
-
+    const dp = [];
+    dp[0] = 1;
+    dp[1] = 1;
+    for(let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
 };
+
+console.log('climbStairs(9) ->', climbStairs(5))
