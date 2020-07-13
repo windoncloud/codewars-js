45c46546953c992cc27b298d1adb796f47b49792
Kingsley <448409934@qq.com>
2020-07-13 08:06:55 +0800

    fet: recall and permute

--- topInterviewQuestions/9_backTracking/3_permute.js
+++ topInterviewQuestions/9_backTracking/3_permute.js
@@ -1,4 +1,4 @@
-// 全排列
+// 46. 全排列
 // 给定一个 没有重复 数字的序列，返回其所有可能的全排列。
 // 示例:
 //     输入: [1,2,3]
@@ -16,5 +16,19 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
-
+    const list = [];
+    backtrack(list, [], nums)
+    return list
 };
+function backtrack(list, tempList, nums) {
+    if (tempList.length === nums.length) return list.push([...tempList]);
+    for(let i = 0; i < nums.length; i++) {
+        if (tempList.includes(nums[i])) continue;
+        tempList.push(nums[i]);
+        backtrack(list, tempList, nums);
+        tempList.pop();
+    }
+}
+
+console.log('permute ->', permute(['a', 'b', 'c']))
+// 链接：https://leetcode-cn.com/problems/permutations/solution/jspython-hui-su-tao-lu-mo-ban-ti-46-quan-pai-lie-b/
