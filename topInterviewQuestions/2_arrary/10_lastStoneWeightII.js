a88e5e2525717e881f1b66ee3fb5eb828a040900
Kingsley <448409934@qq.com>
2020-09-25 09:10:27 +0800

    style： 背包问题

--- topInterviewQuestions/2_arrary/10_lastStoneWeightII.js
+++ topInterviewQuestions/2_arrary/10_lastStoneWeightII.js
@@ -17,5 +17,14 @@
  * @return {number}
  */
 var lastStoneWeightII = function(stones) {
-
+    if (stones === null || stones.length == 0)
+        return 0
+    let sum = 0
+    stones.forEach(element => sum += element)
+    let mid = sum >> 1
+    let dp = new Array(mid + 1).fill(0)
+    for (let i = 0; i < stones.length; i++)
+        for (let j = mid; j >= stones[i]; j--)
+            dp[j] = Math.max(dp[j], stones[i] + dp[j - stones[i]])
+    return (sum - 2 * dp[mid])
 };
